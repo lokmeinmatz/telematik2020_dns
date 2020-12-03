5b6f8eeaf240e4814beb45b746ea4d80bf59b3c7
lokmeinmatz <matze.kind@web.de>
2020-12-03 16:03:56 +0100

    dns server can respond an ip

--- dns_server/src/main.rs
+++ dns_server/src/main.rs
@@ -1,4 +1,6 @@
-use std::net::UdpSocket;
+use std::net::{Ipv4Addr, UdpSocket};
+
+use shared::DNSPacket;
 
 fn main() {
     println!("Hello from DNS Server");
@@ -15,6 +17,16 @@ fn main() {
         };
 
         println!("Received {:?} from {:?}", req_packet, req_sender);
+
+        // test send dns answer
+
+        let answer_packet = DNSPacket {
+            flags_response: true,
+            answer_a: Some([1, 2, 3, 4].into()),
+            ..req_packet
+        };
+        println!("sending response {:?}", answer_packet);
+        shared::send_dns_packet(&socket_in, &answer_packet, req_sender).unwrap();
     }
 
 }
